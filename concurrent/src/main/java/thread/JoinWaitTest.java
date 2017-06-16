db0bcaa4c21f6d46d982c74c63f021d5bf1f6fd9
lihengming <89921218@qq.com>
2017-06-16 17:06:40 +0800

    add > it

---
+++ concurrent/src/main/java/thread/JoinWaitTest.java
@@ -0,0 +1,33 @@
+package thread;
+
+/**
+ * Created by 李恒名 on 2017/6/16.
+ */
+public class JoinWaitTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            try {
+                //模拟子线程需要执行500毫秒
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + " Work End");
+        });
+        thread.start();
+        thread.join(100);//合并到主线程，主线程将等待该子线程执行完毕才会执行，只等待100毫秒，过时不在等。
+
+        System.out.println("Main Thread Work End");
+
+        /**
+         输出：
+             Main Thread Work End
+             Thread-0 Work End
+
+         删除Thread.sleep(500);或者降到100以内：
+             Thread-0 Work End
+             Main Thread Work End
+         */
+    }
+}
