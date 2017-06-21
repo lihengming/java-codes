cd0b33c79033990164d387ab43d56909a909d7bb
lihengming <89921218@qq.com>
2017-06-21 10:53:45 +0800

    update > jvm test code

--- jvm/src/main/java/Test.java
+++ jvm/src/main/java/Test.java
@@ -2,10 +2,6 @@
  * Created by 李恒名 on 2017/6/8.
  */
 public class Test {
-    static {
-        System.out.println("Test Initialize");
-    }
-
     public void say() {
         System.out.println("Hello");
     }
