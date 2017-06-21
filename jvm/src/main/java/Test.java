e3c4eedeed7954befe8ba51d2c6e770869c2ec3c
lihengming <89921218@qq.com>
2017-06-21 10:53:27 +0800

    update > jvm test code

--- jvm/src/main/java/Test.java
+++ jvm/src/main/java/Test.java
@@ -1,11 +1,12 @@
-package classloader;
-
 /**
  * Created by 李恒名 on 2017/6/8.
  */
 public class Test {
+    static {
+        System.out.println("Test Initialize");
+    }
 
-    public void say (){
+    public void say() {
         System.out.println("Hello");
     }
 }
--- jvm/src/main/java/classloader/CustomClassLoader.java
+++ jvm/src/main/java/classloader/CustomClassLoader.java
@@ -1,15 +1,20 @@
 package classloader;
 
-import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.lang.reflect.Method;
 
 /**
  * Created by 李恒名 on 2017/6/8.
  */
 public class CustomClassLoader extends ClassLoader {
     private final String classesDir;
+
+    @Override
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+        return super.loadClass(name,true);
+    }
 
     public CustomClassLoader(String classesDir) {
         this.classesDir = classesDir;
@@ -24,11 +29,11 @@ public class CustomClassLoader extends ClassLoader {
         fileName = fileName + ".class";
 
         try {
-            try (BufferedInputStream bin = new BufferedInputStream(new FileInputStream(classesDir + fileName))) {
+            try (FileInputStream in = new FileInputStream(classesDir + fileName)) {
                 try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
                     byte[] buffer = new byte[1024];
                     int len = 0;
-                    while ((len = bin.read(buffer)) != -1) {
+                    while ((len = in.read(buffer)) != -1) {
                         out.write(buffer,0,len);
                     }
                     byte[] data = out.toByteArray();
@@ -40,5 +45,16 @@ public class CustomClassLoader extends ClassLoader {
             e.printStackTrace();
         }
         return super.findClass(name);
+    }
+
+    public static void main(String[] args) throws ReflectiveOperationException{
+        //1. 将Test.java 编译为Test.class 后复制到 E:\classes 下，当然也可以选择其他目录作为类加载器的classpath。
+        //2. 加载
+        ClassLoader classLoader = new CustomClassLoader("E:\\classes\\");
+        Class<?> clazz = classLoader.loadClass("Test");//如果你的Test在一个包内，需要加上包名，如x.y.z.Test
+        //3. 通过反射调用say()方法
+        Object instance = clazz.newInstance();
+        Method method = clazz.getMethod("say", null);
+        method.invoke(instance);//Hello
     }
 }
