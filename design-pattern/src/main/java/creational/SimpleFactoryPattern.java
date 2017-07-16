b5b50e29ea8decef6eafeba5fae47ec8ec6380ee
lihengming <89921218@qq.com>
2017-07-16 12:19:01 +0800

    update > 简单工厂模式

--- design-pattern/src/main/java/creational/SimpleFactoryPattern.java
+++ design-pattern/src/main/java/creational/SimpleFactoryPattern.java
@@ -6,18 +6,11 @@ package creational;
  * 简单工厂模式
  */
 public class SimpleFactoryPattern {
-
-    public static void main(String[] args) {
-        CarFactory factory = new CarFactory();
-        Car car = factory.getCar("Benz");
-        //Car car = factory.getCar("BMW");
-        car.run();
-    }
-
+    //汽车抽象
     static abstract class Car {
         abstract void run();
     }
-
+    //奔驰汽车
     static class Benz extends Car {
 
         @Override
@@ -25,7 +18,7 @@ public class SimpleFactoryPattern {
             System.out.println("奔驰车在跑");
         }
     }
-
+    //宝马汽车
     static class BMW extends Car {
 
         @Override
@@ -33,7 +26,7 @@ public class SimpleFactoryPattern {
             System.out.println("宝马车在跑");
         }
     }
-
+    //汽车工厂，根据汽车名称创建汽车
     static class CarFactory {
         Car getCar(String name) {
             if (name.equals("Benz")) {
@@ -44,6 +37,13 @@ public class SimpleFactoryPattern {
                 throw new IllegalArgumentException("No support this car");
             }
         }
+    }
+
+    public static void main(String[] args) {
+        CarFactory factory = new CarFactory();
+        Car car = factory.getCar("Benz");
+        //Car car = factory.getCar("BMW");
+        car.run();
     }
 }
 
